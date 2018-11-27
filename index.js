3350746dd72043f91dbed8c595ffa1dec9ed5183
Yerim Hu <yerimhu88@gmail.com>
2018-11-27 05:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var musicians = [];
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   for (var i = 0; i < musicians.length; i++) {
-    musicians[i]
+    for (var i = 0; i < instruments.length; i++) {
+      return musicians[i] + " plays " + instruments[i];
+    }
   }
 }
