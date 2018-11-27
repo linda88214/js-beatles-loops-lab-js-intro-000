c6eaa84260295abcb5b612570702530a3f7b5c31
Yerim Hu <yerimhu88@gmail.com>
2018-11-27 05:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var empty = [];
   for (var i = 0; i < musicians.length; i++) {
-      return empty.push(musicians[i] + " plays " + instruments[i]);
+     empty.push(musicians[i] + " plays " + instruments[i]);
   }
+  return empty
 }
